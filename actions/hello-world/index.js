8306c7071cde67c4b3c71ff478ca1ae1e84fa425
carlosmancetech <carlos@mance.tech>
2024-09-05 11:04:15 +0200

    recolocados archivos en carpeta .github

--- actions/hello-world/index.js
+++
@@ -1,9 +0,0 @@
-// .github/actions/hello-world/index.js
-const actionsCore = require('@actions/core');
-
-try {
-  const nameToGreet = actionsCore.getInput('who-to-greet');
-  console.log(`Hola, ${nameToGreet}! Se ha realizado un push a la branch main en este momento`);
-} catch (error) {
-  actionsCore.setFailed(`Se ha producido un error: ${error.message}`);
-}
