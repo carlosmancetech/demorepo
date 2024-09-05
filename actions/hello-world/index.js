490e8eab9c696948ca4a142e2675d39aa9ee2cd0
carlosmancetech <carlos@mance.tech>
2024-09-05 10:29:22 +0200

    Update index.js

--- actions/hello-world/index.js
+++ actions/hello-world/index.js
@@ -3,7 +3,7 @@ const actionsCore = require('@actions/core');
 
 try {
   const nameToGreet = actionsCore.getInput('who-to-greet');
-  console.log(`Hola, ${nameToGreet}!`);
+  console.log(`Hola, ${nameToGreet}! Se ha realizado un push a la branch main en este momento`);
 } catch (error) {
   actionsCore.setFailed(`Se ha producido un error: ${error.message}`);
 }
